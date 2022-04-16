136c2b50bb6bf53d2270480d72922e3c2f181c2e
59114_Chetana <deslechetna1999@gmail.com>
2022-04-16 19:06:18 +0530

    [chetana] Added :Monthly Payment

---
+++ LogicalProgramming/MonthlyPayment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class MonthlyPayment
+    {
+        public void Payment()
+        {
+            double payment, PrincipalAmount, r, n, Rate;
+            int years;
+            Console.WriteLine("Enter the years");
+            years = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" Enter the PrincipalAmount");
+            PrincipalAmount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the CompoundInterest Rate ");
+            Rate = Convert.ToDouble(Console.ReadLine());
+            n = 12 * years;
+            r = (Rate) / (12 * 100);
+            payment = ((PrincipalAmount * r) / (1 - Math.Pow((1 + r), -n)));
+            Console.WriteLine("Monthly Payment :" + payment);
+        }
+    }
+}
+
