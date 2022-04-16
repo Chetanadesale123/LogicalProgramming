4868a2edaef57ee55f526bc37047cd832c895ae7
59114_Chetana <deslechetna1999@gmail.com>
2022-04-16 18:39:36 +0530

    [chetana] Added :Day Of Week Code

---
+++ LogicalProgramming/DayOfWeek.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class DayOfWeek
+    {
+        public void Day()
+        {
+            Console.WriteLine("Enter the Month 1 to 12");
+            int month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Day 1 to 7");
+            int Day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Year 4 Digits");
+            int Year = Convert.ToInt32(Console.ReadLine());
+            int y0 = Year - (14 - month) / 12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = month + 12 * ((14 - month) / 12) - 2;
+            int d0 = (Day + x + 31 * m0 / 12) % 7;
+            switch (d0)
+            {
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+            }
+        }
+    }
+}
+    
+
--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         Console.WriteLine("Welcome to Logical Programs");
         bool end = true;
-        Console.WriteLine("1.Fibonacci series\n 2.Perfect No\n 3.Prime Number\n 4.Reverse a no \n 5 Return Notes\n  6. End the Program");
+        Console.WriteLine("1.Fibonacci series\n 2.Perfect No\n 3.Prime Number\n 4.Reverse a no \n 5 Return Notes\n 6.Day of Week\n  7. End the Program");
         while (end == true)
         {
             Console.WriteLine("Take an option to execute");
@@ -36,6 +36,10 @@ class Program
                     notes.ReturnNotes();
                     break;
                 case 6:
+                    DayOfWeek day = new DayOfWeek();
+                    day.Day();
+                    break;
+                case 7:
                     end = false;
                     break;
                 default:
