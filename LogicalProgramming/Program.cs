391c53cd66ee3926ea824a31ec22053b262089a1
59114_Chetana <deslechetna1999@gmail.com>
2022-04-16 18:51:53 +0530

    [chetana] Added :Temperature Conversion code

--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -7,7 +7,7 @@ class Program
     {
         Console.WriteLine("Welcome to Logical Programs");
         bool end = true;
-        Console.WriteLine("1.Fibonacci series\n 2.Perfect No\n 3.Prime Number\n 4.Reverse a no \n 5 Return Notes\n 6.Day of Week\n  7. End the Program");
+        Console.WriteLine("1.Fibonacci series\n 2.Perfect No\n 3.Prime Number\n 4.Reverse a no \n 5 Return Notes\n 6.Day of Week\n 7.Temperature Conversion\n 8. End the Program");
         while (end == true)
         {
             Console.WriteLine("Take an option to execute");
@@ -40,6 +40,11 @@ class Program
                     day.Day();
                     break;
                 case 7:
+                    TemperatureConversion conv = new TemperatureConversion();
+                    conv.CelToFeh();
+                    conv.FehToCel();
+                    break;
+                case 8:
                     end = false;
                     break;
                 default:
---
+++ LogicalProgramming/TemperatureConversion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class TemperatureConversion
+    {
+        public void CelToFeh()
+        {
+            float Fahrenheit, Celsius;
+            Console.WriteLine("Enter the Temperature In Celcius ");
+            Celsius = float.Parse(Console.ReadLine());
+
+            Fahrenheit = (Celsius * 9.0f / 5.0f) + 32.0f;
+            Console.WriteLine("Temperature in Celsius to Fahrenheit is : " + Fahrenheit);
+        }
+        public void FehToCel()
+        {
+            float Fahrenheit, Celsius;
+            Console.WriteLine("Enter the Temperature In Fahrenheit ");
+            Fahrenheit = float.Parse(Console.ReadLine());
+            Celsius = (Fahrenheit - 32.0f) * 5.0f / 9.0f;
+            Console.WriteLine("Temperature in Fahrenheit to Celsius is : " + Celsius);
+        }
+    }
+}
+    
+
