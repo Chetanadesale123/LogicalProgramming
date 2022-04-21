34a636d298fe1a8a8f798e7174b763b54983c6ca
59114_Chetana <deslechetna1999@gmail.com>
2022-04-21 12:44:06 +0530

    [Chetana] Refactor:Coupon no code

--- LogicalProgramming/CouponNumbers.cs
+++ LogicalProgramming/CouponNumbers.cs
@@ -10,36 +10,36 @@ namespace LogicalProgramming
     {
         public void CouponNo()
         {
-			int distinct = 0, count = 0, coupon_no;
-			Console.Write("Enter the number of distinct coupons:");
-			coupon_no = Convert.ToInt32(Console.ReadLine());
-			bool[] is_collected = new bool[coupon_no];
-			while (distinct < coupon_no)
-			{
-				Random random = new Random();
-				int option = random.Next(1, 100);
-				int new_coupon = (int)(random.NextDouble() * coupon_no);
-				count++;
-				if (!is_collected[new_coupon])
-				{
-					distinct++;
-					is_collected[new_coupon] = true;
-				}
-			}
+            int count = 0, n;
+            Console.WriteLine("Enter the  coupon number");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n > 0)
+            {
+                Random random = new Random();
+                int check = random.Next(1, 99);
+                if (check == n)//21!=25 25==25
+                {
+                    Console.WriteLine("Congrats!!got the coupon number in first attempt");
+                    break;
+                }
+                count++;//1 2
+                Console.WriteLine("count to get coupon number is:" + count);
+               // n--;
+            }
+            Console.WriteLine("to get distinct coupon number the number of times random number used:" + count);//total no of count=2
 
-			Console.WriteLine("Total random number needed to have all distinct coupons : " + count);
-		}
-	}
+        }
+    }
 }
 
-        
-            
-            
-    
-         
-        
-		
-			
-		
-    
+
+
+
+
+
+
+
+
+
+
 
